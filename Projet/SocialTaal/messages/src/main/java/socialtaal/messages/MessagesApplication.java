e6987a360682ec9f468041b9aad363486e4e2b36
ThomasJohnen <thomas.johnen@hotmail.be>
2024-07-27 22:11:53 +0200

    modify : add annotations in application to be discoverable

--- Projet/SocialTaal/messages/src/main/java/socialtaal/messages/MessagesApplication.java
+++ Projet/SocialTaal/messages/src/main/java/socialtaal/messages/MessagesApplication.java
@@ -2,8 +2,12 @@ package socialtaal.messages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MessagesApplication {
 
     public static void main(String[] args) {
--- Projet/SocialTaal/search/src/main/java/socialtaal/search/SearchApplication.java
+++ Projet/SocialTaal/search/src/main/java/socialtaal/search/SearchApplication.java
@@ -2,8 +2,12 @@ package socialtaal.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SearchApplication {
 
     public static void main(String[] args) {
